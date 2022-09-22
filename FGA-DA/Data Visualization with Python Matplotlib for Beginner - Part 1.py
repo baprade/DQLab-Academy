baedd8a5bcc3de6b6b1562cd089838804bb99faa
Bagas Prasta Dewa <me@bagaspras.my.id>
2022-09-22 12:48:44 +0700

    Create

---
+++ FGA-DA/Data Visualization with Python Matplotlib for Beginner - Part 1.py
@@ -0,0 +1,40 @@
+# Plot Pertama dengan Matplotlib
+# Membuat Data Agregat
+import pandas as pd
+import datetime
+
+dataset = pd.read_csv('https://storage.googleapis.com/dqlab-dataset/retail_raw_reduced.csv')
+dataset['order_month'] = dataset['order_date'].apply(lambda x: datetime.datetime.strptime(x, "%Y-%m-%d").strftime('%Y-%m'))
+dataset['gmv'] = dataset['item_price']*dataset['quantity']
+
+monthly_amount = dataset.groupby('order_month')['gmv'].sum().reset_index()
+print (monthly_amount)
+
+
+# Plot Pertama: Membuat Line Chart Trend Pertumbuhan GMV
+import pandas as pd
+import datetime
+import matplotlib.pyplot as plt
+
+dataset = pd.read_csv('https://storage.googleapis.com/dqlab-dataset/retail_raw_reduced.csv')
+dataset['order_month'] = dataset['order_date'].apply(lambda x: datetime.datetime.strptime(x, "%Y-%m-%d").strftime('%Y-%m'))
+dataset['gmv'] = dataset['item_price']*dataset['quantity']
+
+monthly_amount = dataset.groupby('order_month')['gmv'].sum().reset_index()
+
+plt.plot(monthly_amount['order_month'], monthly_amount['gmv'])
+plt.show()
+
+
+# Cara Alternatif: Fungsi .plot() pada pandas Dataframe
+import pandas as pd
+import datetime
+import matplotlib.pyplot as plt
+
+dataset = pd.read_csv('https://storage.googleapis.com/dqlab-dataset/retail_raw_reduced.csv')
+dataset['order_month'] = dataset['order_date'].apply(lambda x: datetime.datetime.strptime(x, "%Y-%m-%d").strftime('%Y-%m'))
+dataset['gmv'] = dataset['item_price']*dataset['quantity']
+
+dataset.groupby(['order_month'])['gmv'].sum().plot()
+plt.show()
+
