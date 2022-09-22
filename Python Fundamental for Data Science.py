51cc3fafd2b8328f098cc80077fd75f6f49cce0c
Bagas Prasta Dewa <me@bagaspras.my.id>
2022-09-22 11:30:53 +0700

    Update and rename Python Fundamental for Data Science to Python Fundamental for Data Science.py

---
+++ Python Fundamental for Data Science.py
@@ -0,0 +1,27 @@
+# NESTED IF
+i=2
+if (i<7):
+	print ("nilai i kurang dari 7")
+	if (i<3):
+		print ("nilai i kurang dari 7 dan kurang dari 3")
+	else:
+		print ("nilai i kurang dari 7 tapi lebih dari 3")	
+
+# Praktik Operasi Matematika
+a=10
+b=5
+selisih = a-b
+jumlah = a+b
+kali = a*b
+bagi = a/b
+print("Hasil penjumlahan a dan b adalah", jumlah)
+print("Selisih a dan b adalah :",selisih)
+print("Hasil perkalian a dan b adalah :",kali)
+print("Hasil pembagian a dan b adalah:",bagi)
+
+# Operasi modulus
+c=10
+d=3
+
+modulus=c%d
+print("Hasil modulus",modulus)
